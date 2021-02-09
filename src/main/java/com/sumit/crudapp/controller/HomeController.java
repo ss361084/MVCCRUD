5d67628a1b364f47f4d97dfc44ad4e9ff28a27c3
Sumit Sharma <ss361084@gmail.com>
2021-02-09 23:12:32 +0530

    1.Add label and Messages properties 2.Add GetStudentList() method

--- src/main/java/com/sumit/crudapp/controller/HomeController.java
+++ src/main/java/com/sumit/crudapp/controller/HomeController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -20,8 +22,11 @@ public class HomeController {
 	private StudentService studentService;
 	
 	@RequestMapping(value = {"/"})
-	public String getHomePage() {
-		return "Home";
+	public ModelAndView getHomePage(Model model) {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("listStudent", studentService.getAllStudent());
+		mv.setViewName("Home");
+		return mv;
 	}
 	
 	@RequestMapping(value = {"/addstudentdetail"})
@@ -39,5 +44,11 @@ public class HomeController {
 		}
 		studentService.saveStudent(student);
 		return "redirect:/";
+	}
+	
+	@GetMapping(value = {"/update/{studId}"})
+	public String getStudentData(@PathVariable(value = "studId") long studId, Model model) {
+		
+		return "";
 	}
 }
